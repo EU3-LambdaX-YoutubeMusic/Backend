d00f87578865dd3287688f46bafa1c34cb788e04
Akere Adetola <adetolaakere1@gmail.com>
2020-05-12 15:26:57 +0100

    user model

---
+++ resources/users/users.models.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+const UserSchema = Schema({
+  firstName: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 50,
+  },
+  lastName: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 50,
+  },
+  email: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 50,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 80,
+  },
+});
+
+const UserModel = mongoose.model('User', UserSchema);
+module.exports = UserModel;
