aff04aee2cec6b558f411c230f0c102fe5356209
Oluwajoba <oluwajobabello@gmail.com>
2020-05-14 00:01:39 +0100

    add mainRouter for playlist api / index.js

--- resources/index.js
+++ resources/index.js
@@ -1,9 +1,11 @@
-const express = require('express');
+const express = require("express");
 
 const mainRouter = express.Router();
 
-const users = require('./users/users.routes');
+const users = require("./users/users.routes");
+const playlist = require("./playlist/playlist.routes");
 
-mainRouter.use('/api/v1/users', users);
+mainRouter.use("/api/v1/users", users);
+mainRouter.use("/api/v1/playlist", playlist);
 
 module.exports = mainRouter;
