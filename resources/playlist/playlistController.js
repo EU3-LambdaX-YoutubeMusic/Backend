14114c085a9fa0837766df28a5e1f8e76e00b771
Oluwajoba <oluwajobabello@gmail.com>
2020-05-14 06:53:18 +0100

    check user is login before posting / playlistController.js

--- resources/playlist/playlistController.js
+++ resources/playlist/playlistController.js
@@ -22,13 +22,28 @@ const addMusic = async (req, res) => {
   try {
     const { artist, title, video_url, favourite } = req.body;
     // const existingUser = await Playlist.findOne({ user_id });
-    const playlist = new Playlist({
-      artist,
-      title,
-      video_url,
-      favourite,
-    });
-    await playlist.save();
+    const { token } = req.headers;
+    if (token) {
+      const playlist = new Playlist({
+        artist,
+        title,
+        video_url,
+        favourite,
+      });
+      await playlist.save();
+
+      return res.status(201).json({
+        status: 201,
+        message: "Playlist created successfully",
+        playlist: playlist,
+      });
+    } else {
+      return res.status(401).json({
+        status: 401,
+        message: "Login to add music",
+      });
+    }
+
     // });
   } catch (error) {
     return res.status(500).json({
