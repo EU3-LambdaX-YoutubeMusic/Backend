27e932f51cfc0f4bea07e854dc4bedbde5df47aa
Oluwajoba <oluwajobabello@gmail.com>
2020-05-14 13:42:32 +0100

    update getAllMusic controller to check that user is logged in / only find all music link with userId / playlistController.js

--- resources/playlist/playlistController.js
+++ resources/playlist/playlistController.js
@@ -1,13 +1,25 @@
 const Playlist = require("./playlist.models");
+var mongoose = require("mongoose");
 
 const getAllMusic = async (req, res) => {
   try {
-    Playlist.find({}).then(function (playlist) {
-      res.status(200).json({
-        status: 200,
-        playlist,
+    const userId = req.userId;
+    const { token } = req.headers;
+    if (token) {
+      await Playlist.find({
+        user_id: { $in: [mongoose.Types.ObjectId(userId)] },
+      }).then(function (playlist) {
+        res.status(200).json({
+          status: 200,
+          playlist,
+        });
       });
-    });
+    } else {
+      return res.status(401).json({
+        status: 401,
+        message: "Login to see your playlist",
+      });
+    }
   } catch (error) {
     return res.status(500).json({
       status: 500,
