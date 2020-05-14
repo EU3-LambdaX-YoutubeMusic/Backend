dfe2b8ea626dcd4d9baed24bf43fb33c4214acb5
Oluwajoba <oluwajobabello@gmail.com>
2020-05-14 09:47:30 +0100

    add validateToken middleware to addMusic endpoint / playlist.routes.js

--- resources/playlist/playlist.routes.js
+++ resources/playlist/playlist.routes.js
@@ -3,10 +3,11 @@ const express = require("express");
 const router = express.Router();
 
 const playlistController = require("./playlistController");
+const { validateToken } = require("./middlewares");
 
 const { getAllMusic, addMusic } = playlistController;
 
 router.get("/getAllMusic", getAllMusic);
-router.post("/addMusic", addMusic);
+router.post("/addMusic", validateToken, addMusic);
 
 module.exports = router;
