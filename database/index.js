90fda3bbf0620c06f53a0cbef4edd1e8b7fdaa5f
Akere Adetola <adetolaakere1@gmail.com>
2020-05-14 01:34:27 +0100

    update readme file

--- database/index.js
+++ database/index.js
@@ -16,7 +16,6 @@ module.exports = async () => {
       dbName: 'youtube_music'
     });
     console.log("Connected to Database");
-  
   } catch (e) {
     console.log(`Connection to database failed: ${e.message}`);
   }
