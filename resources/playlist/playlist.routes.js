2f9c7fde735cafff97e473d94acbc5d4f9a18446
Oluwajoba <oluwajobabello@gmail.com>
2020-05-20 11:35:29 +0100

    add route for  "deleteMusic" endpoint / playlist.routes.js

--- resources/playlist/playlist.routes.js
+++ resources/playlist/playlist.routes.js
@@ -5,10 +5,11 @@ const router = express.Router();
 const playlistController = require("./playlistController");
 const { validateToken } = require("../../utils/validateToken");
 
-const { getAllMusic, addMusic, updateMusic } = playlistController;
+const { getAllMusic, addMusic, updateMusic, deleteMusic } = playlistController;
 
 router.get("/getAllMusic", validateToken, getAllMusic);
 router.post("/addMusic", validateToken, addMusic);
 router.put("/updateMusic", validateToken, updateMusic);
+router.delete("/deleteMusic", validateToken, deleteMusic);
 
 module.exports = router;
